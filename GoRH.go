976dd61b1f5e93b61dcbb8f02d72ee9c4ae233d2
Emerson Drapac <edrapac@netspi.com>
2020-08-10 20:27:13 -0500

    yolo trading mode enabled

--- GoRH.go
+++ GoRH.go
@@ -4,31 +4,49 @@ import (
 	"net/http"
 	"io/ioutil"
 	"regexp"
-	"bufio"
-	"os"
+	"math/rand"
+	"time"
 	"strings"
 )
-
-func main() {
-	base_url := "https://robinhood.com/stocks/" // base landing page for RH's stock information
-	fmt.Println("Enter a Stock's ticker symbol here. Try AAPL")
-	reader := bufio.NewReader(os.Stdin)
-	fragment, _ := reader.ReadString('\n')
-	new_fragment := strings.ToUpper(strings.TrimSpace(fragment))
-	base_url += new_fragment // kind of an ugly way to do it but it gets the job done!
-	
-
-	response, err := http.Get(base_url)
-
-	if err != nil {
-		panic(err)
+func fragment() string {
+	base_url := "https://robinhood.com/stocks/"
+	fragment := ""
+	for i := 0; i < 4; i++ {
+		rand.Seed((time.Now().UnixNano()))
+    	min := 65
+		max := 90
+		newascii := rand.Intn(max - min)+min
+		fragment += string(newascii)
 	}
+	new_fragment := strings.ToUpper(fragment)
+	base_url += new_fragment // kind of an ugly way to do it but it gets the job done!
+	return base_url
+}
+func body(url string) string {
+	response, err := http.Get(url)
 	bytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		panic(err)
 	}
+	fullcorp := string(bytes)
+	return fullcorp
+}
 
-	fullcorpus := (string(bytes))
+func main() {
+	 // base landing page for RH's stock information
+	fmt.Println("Randomly selecting a stock")
+	new_fragment := ""
+	fullcorpus := ""
+	for {
+		new_url := fragment()
+		new_fragment = new_url 
+		fullcorpus = body(new_url)
+		// fmt.Println(new_url) lol
+		if len(fullcorpus) > 89280 {
+			break
+		}
+	}
+	
 	price, _ := regexp.Compile(`\\"price\\",\\"[0-9]+.[0-9]+`)
 	hold, _ := regexp.Compile(`Hold</div>.*[0-9]+%`)
 	holdpercent, _ := regexp.Compile(`[0-9]+%</span>`)
